6a29d2f3ddaedffe6f5bef1b8b216195ed0dae8c
futuping <45912467+futuping@users.noreply.github.com>
2020-02-29 01:27:20 +0800

    Add files via upload

---
+++ Casks/hallelujahinputmethod.rb
@@ -0,0 +1,20 @@
+cask 'hallelujahinputmethod' do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/futuping/homebrew-plus/releases/download/v1.5.0/hallelujahInputMethod.zip"
+  name 'hallelujahInputMethod'
+  homepage 'https://github.com/dongyuwei/hallelujahIM'
+
+  pkg 'hallelujahInputMethod.pkg'
+
+  uninstall pkgutil: [
+                       'github.dongyuwei.inputmethod.hallelujahInputMethod',
+                     ],
+            delete:  '/Library/Input Methods/hallelujah.app'
+
+  zap trash: [
+               '~/Library/Caches/github.dongyuwei.inputmethod.hallelujahInputMethod',
+               '~/Library/Preferences/github.dongyuwei.inputmethod.hallelujahInputMethod.plist',
+             ]
+end
